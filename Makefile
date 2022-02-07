c471a7303f0ee67c12dd0f67e098a9c80feab05f
elpinal <elpinal@users.noreply.github.com>
2022-02-07 11:55:17 +0900

    Add init.el

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
 PREFIX=$(HOME)
 
 install:
-	cp vim/.vimrc $(HOME)/.vimrc
+	cp vim/.vimrc $(PREFIX)/.vimrc
+
+	touch $(PREFIX)/.emacs.d/custom.el
+	cp emacs/init.el $(PREFIX)/.emacs.d/init.el
 
 .PHONY: install
---
+++ emacs/init.el
@@ -0,0 +1,83 @@
+(setq debug-on-error t)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
+
+(eval-and-compile
+  (customize-set-variable
+   'package-archives '(("org" . "https://orgmode.org/elpa/")
+                       ("melpa" . "https://melpa.org/packages/")
+                       ("gnu" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless (package-installed-p 'leaf)
+    (package-refresh-contents)
+    (package-install 'leaf))
+
+  (leaf leaf-keywords
+    :ensure t
+    :bind
+      ("C-h" . delete-backward-char)
+      ("M-?" . help-for-help)
+      ("C-z" . help-for-help)
+      ("C-x M-q" . quoted-insert)
+      ("C-q" . move-beginning-of-line)
+      ("M-h" . backward-kill-word)
+      ("<left>" . windmove-left)
+      ("<down>" . windmove-down)
+      ("<up>" . windmove-up)
+      ("<right>" . windmove-right)
+      ("M-c" . delete-window)
+    :config
+      (leaf-keywords-init)))
+
+(menu-bar-mode -1)
+
+; For agda-mode.
+(set-language-environment "English")
+
+(setq require-final-newline t)
+
+(leaf undo-tree
+  :ensure t
+  :bind ("M-r" . undo-tree-redo)
+  :init (global-undo-tree-mode))
+
+(leaf smartparens
+  :ensure t
+  :init (smartparens-global-mode t))
+
+(leaf racket-mode
+  :ensure t)
+
+(leaf clojure-mode
+  :ensure t
+  :config
+    (add-hook 'clojure-mode-hook #'smartparens-strict-mode))
+
+(leaf sml-mode
+  :ensure t)
+
+(leaf web-mode
+  :ensure t)
+
+(leaf magit
+  :ensure t
+  :bind ("C-x m" . magit))
+
+(leaf ddskk
+  :ensure t)
+
+(leaf dracula-theme
+  :ensure t
+  :init
+    (load-theme 'dracula t)
+    (enable-theme 'dracula))
+
+(leaf exec-path-from-shell
+  :ensure t
+  :init
+    (exec-path-from-shell-initialize))
+
+(if (executable-find "agda-mode")
+  (load-file (let ((coding-system-for-read 'utf-8))
+                  (shell-command-to-string "agda-mode locate"))))
